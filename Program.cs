b673061d6cba4f12566089c72ede6927f0372627
Toluwani Morohunfola <taemorohunfola@outlook.com>
2025-10-14 08:11:34 +0100

    Added C# project and initial code

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Budget Tracker App!");
+
+// user name
+string userName = "Alice";
+
+
+// overall budget
+int overallBudget = 1000;
+
+
+// expenses
+double foodExpense = 250.75;
+double transportExpense = 100.50;
+double entertainmentExpense = 150.00;
+
+// opt-in for notifications
+bool notificationsOptIn = true;
+
+Console.WriteLine($"Hello, {userName}, your overall budget is ${overallBudget}. After expenses, you have ${overallBudget - (foodExpense + transportExpense + entertainmentExpense)} left.");
+if (notificationsOptIn)
+{
+    Console.WriteLine("You have opted in for notifications.");
+}
+else
+{
+    Console.WriteLine("You have opted out of notifications.");
+}
