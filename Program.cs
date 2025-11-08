6f1fb86f1044198c6616bc7830268b2f080793c4
Toluwani Morohunfola <taemorohunfola@gmail.com>
2025-11-08 18:46:44 +0000

    used while loops and refactored using for loops.

--- Program.cs
+++ Program.cs
@@ -3,19 +3,38 @@ Console.WriteLine("Welcome to Budget Tracker App!");
 // user name
 string userName = "Alice";
 // overall budget
-int overallBudget = 1800;
-// specific budgets
-int foodBudget = 200;
-int transportBudget = 300;
-int entertainmentBudget = 100;
-// expenses
-double foodExpense = 250.75;
-double transportExpense = 100.50;
-double entertainmentExpense = 150.00;
+double overallBudget = 1800;
+// specific budgets in a list, food, transport and entertainment
+List<double> budgets = new List<double> { 200, 300, 100 };
+// expenses in a list, food transport and entertainment
+List<double> expenses = new List<double> { 250.75, 100.50, 150.00 };
+
+// function to calculate total expenses
+double CalculateTotalExpenses(List<double> expenses)
+{
+    // for loop to calculate total expenses
+    // first initialise the varaible that will be the result of the for loop
+    double totalExpenses = 0;
+    for (int i = 0; i < expenses.Count; i++)
+    {
+        totalExpenses = totalExpenses + expenses[i];
+    }
+}
+
+
+// while loop to ensure Alice doesn't go over the spending limit
+double spendingLimit = 900;
+double totalSpending = 0;
+int i = 0;
+while (totalSpending < spendingLimit)
+{
+    totalSpending = totalSpending + expenses[i];
+}
+
 // opt-in for notifications
 bool notificationsOptIn = true;
 // Display user information
-Console.WriteLine($"Hello, {userName}, your overall budget is ${overallBudget}. After expenses, you have ${overallBudget - (foodExpense + transportExpense + entertainmentExpense)} left.");
+Console.WriteLine($"Hello, {userName}, your overall budget is ${overallBudget}. After expenses, you have ${overallBudget - totalExpenses} left.");
 if (notificationsOptIn)
 {
     Console.WriteLine("You have opted in for notifications.");
